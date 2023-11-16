1f88cebfa0b500dc8d084c488469760f611a9108
franneck94 <schaffranek.jan@web.de>
2023-11-16 11:44:14 +0100

    it compiles :)

--- src/Utils/UtilsGui.h
+++ src/Utils/UtilsGui.h
@@ -30,47 +30,47 @@ void DrawButton(ActionState &action_state,
                 std::string_view text,
                 const ImVec2 button_size = DEFAULT_BUTTON_SIZE);
 
-template <uint32_t N>
-void DrawMovingButtons(const std::array<MoveABC *, N> &moves, bool &move_ongoing, uint32_t &move_idx)
-{
-    bool was_already_ongoing = move_ongoing;
-    if (was_already_ongoing)
-        ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1F, 0.9F, 0.1F, 1.0F));
-    if (ImGui::Button(moves[move_idx]->Name(), DEFAULT_BUTTON_SIZE))
-    {
-        if (!move_ongoing)
-        {
-            if (!moves[move_idx]->is_distance_based)
-                moves[move_idx]->Execute();
-            move_ongoing = true;
-        }
-        else
-        {
-            move_ongoing = false;
-            CancelMovement();
-        }
-    }
-    if (was_already_ongoing)
-        ImGui::PopStyleColor();
+// template <uint32_t N>
+// void DrawMovingButtons(const std::array<MoveABC *, N> &moves, bool &move_ongoing, uint32_t &move_idx)
+// {
+//     bool was_already_ongoing = move_ongoing;
+//     if (was_already_ongoing)
+//         ImGui::PushStyleColor(ImGuiCol_Button, ImVec4(0.1F, 0.9F, 0.1F, 1.0F));
+//     if (ImGui::Button(moves[move_idx]->Name(), DEFAULT_BUTTON_SIZE))
+//     {
+//         if (!move_ongoing)
+//         {
+//             if (!moves[move_idx]->is_distance_based)
+//                 moves[move_idx]->Execute();
+//             move_ongoing = true;
+//         }
+//         else
+//         {
+//             move_ongoing = false;
+//             CancelMovement();
+//         }
+//     }
+//     if (was_already_ongoing)
+//         ImGui::PopStyleColor();
 
-    if (ImGui::Button("Prev.", ImVec2(SKIP_BUTTON_X, SKIP_BUTTON_Y)))
-    {
-        if (move_idx > 0)
-            --move_idx;
-        else
-            move_idx = moves.size() - 1;
-        move_ongoing = false;
-    }
-    ImGui::SameLine();
-    if (ImGui::Button("Next", ImVec2(SKIP_BUTTON_X, SKIP_BUTTON_Y)))
-    {
-        if (move_idx < moves.size() - 1)
-            ++move_idx;
-        else
-            move_idx = 0;
-        move_ongoing = false;
-    }
-}
+//     if (ImGui::Button("Prev.", ImVec2(SKIP_BUTTON_X, SKIP_BUTTON_Y)))
+//     {
+//         if (move_idx > 0)
+//             --move_idx;
+//         else
+//             move_idx = moves.size() - 1;
+//         move_ongoing = false;
+//     }
+//     ImGui::SameLine();
+//     if (ImGui::Button("Next", ImVec2(SKIP_BUTTON_X, SKIP_BUTTON_Y)))
+//     {
+//         if (move_idx < moves.size() - 1)
+//             ++move_idx;
+//         else
+//             move_idx = 0;
+//         move_ongoing = false;
+//     }
+// }
 
 void PlotRectangleLine(const GW::GamePos &player_pos,
                        const GW::GamePos &p1,
--- src/stdafx.h
+++ src/stdafx.h
@@ -1,9 +1,34 @@
 #pragma once
 
+#ifndef _CRT_SECURE_NO_WARNINGS
+# define _CRT_SECURE_NO_WARNINGS 1
+#endif
+
+#include <cmath>
+#include <cstdio>
+#include <cstdint>
+#include <cstring>
+
+#include <map>
+#include <atomic>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <algorithm>
 #include <functional>
+#include <unordered_map>
+#include <utility>
+
+#ifndef NOMINMAX
+# define NOMINMAX
+#endif
+
+#ifndef WIN32_LEAN_AND_MEAN
+# define WIN32_LEAN_AND_MEAN
+#endif
 
 #include <Windows.h>
-#include <windowsx.h>
+#include <ShellApi.h>
 
 #include <GWCA/GameContainers/Array.h>
 
