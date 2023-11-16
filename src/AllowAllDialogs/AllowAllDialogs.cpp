bbbfe0e0eeb75be8c7945de393d1c9eb38330457
franneck94 <schaffranek.jan@web.de>
2023-11-16 08:59:41 +0100

    test stuff

--- src/AllowAllDialogs/AllowAllDialogs.cpp
+++ src/AllowAllDialogs/AllowAllDialogs.cpp
@@ -1,17 +1,55 @@
+#include <map>
+#include <vector>
+
 #include "AllowAllDialogs.h"
 
+#include <GWCA/Managers/AgentMgr.h>
+#include <GWCA/Managers/CtoSMgr.h>
 #include <GWCA/Utilities/Hooker.h>
 #include <GWCA/Utilities/Scanner.h>
 
 namespace
 {
-using IsDialogAvailable_pt = bool(__cdecl *)(unsigned int dialog_id);
-IsDialogAvailable_pt IsDialogAvailable_Func = nullptr;
-IsDialogAvailable_pt IsDialogAvailable_Ret = nullptr;
+using DialogCallback = GW::HookCallback<uint32_t>;
+
+uint32_t last_dialog_id = 0;
+std::vector<std::unordered_map<GW::HookEntry *, GW::CtoS::PacketCallback>> packets_callbacks;
+std::unordered_map<GW::HookEntry *, DialogCallback> OnDialog_callbacks;
+
+using SendDialog_pt = bool(__cdecl *)(unsigned int dialog_id);
+using SendPacket_pt = void(__cdecl *)(uint32_t context, uint32_t size, void *packet);
+
+SendDialog_pt SendDialog_Func = nullptr;
+SendDialog_pt SendDialog_Ret = nullptr;
+
+SendDialog_pt SendSignpostDialog_Func = nullptr;
+SendDialog_pt SendSignpostDialog_Ret = nullptr;
+
+SendPacket_pt SendPacket_Func = 0;
+SendPacket_pt SendPacket_Ret = 0;
 
 bool OnIsDialogAvailable(unsigned int)
 {
     // Any other logic here.
+    return true;
+}
+
+bool CtoSHandler_Func(uint32_t context, uint32_t size, void *packet)
+{
+    GW::HookBase::EnterHook();
+    GW::HookStatus status;
+    uint32_t header = *(uint32_t *)packet;
+    if (header < packets_callbacks.size())
+    {
+        for (auto &it : packets_callbacks[header])
+        {
+            it.second(&status, packet);
+            ++status.altitude;
+        }
+    }
+    if (!status.blocked)
+        SendPacket_Ret(context, size, packet);
+    GW::HookBase::LeaveHook();
     return true;
 }
 } // namespace
@@ -24,8 +62,12 @@ DLLAPI ToolboxPlugin *ToolboxPluginInstance()
 
 void AllowAllDialogs::SignalTerminate()
 {
-    if (IsDialogAvailable_Func)
-        GW::HookBase::DisableHooks(IsDialogAvailable_Func);
+    if (SendDialog_Func)
+        GW::HookBase::DisableHooks(SendDialog_Func);
+    if (SendPacket_Func)
+        GW::HookBase::DisableHooks(SendPacket_Func);
+    if (SendSignpostDialog_Func)
+        GW::HookBase::DisableHooks(SendSignpostDialog_Func);
 }
 
 bool AllowAllDialogs::CanTerminate()
@@ -36,28 +78,72 @@ bool AllowAllDialogs::CanTerminate()
 void AllowAllDialogs::Terminate()
 {
     ToolboxPlugin::Terminate();
-    GW::HookBase::RemoveHook(IsDialogAvailable_Func);
+    GW::HookBase::RemoveHook(SendDialog_Func);
+    GW::HookBase::RemoveHook(SendPacket_Func);
+    GW::HookBase::RemoveHook(SendSignpostDialog_Func);
     GW::HookBase::Deinitialize();
 }
 
 void AllowAllDialogs::Initialize(ImGuiContext *ctx, ImGuiAllocFns fns, HMODULE toolbox_dll)
 {
     ToolboxPlugin::Initialize(ctx, fns, toolbox_dll);
-
     GW::HookBase::Initialize();
     GW::Scanner::Initialize(toolbox_dll);
-    const uintptr_t address = GW::Scanner::Find("\x25\x00\x00\x00\xFF\x74\x02", "xxxxxxx", 0xd);
-    IsDialogAvailable_Func = reinterpret_cast<IsDialogAvailable_pt>(GW::Scanner::FunctionFromNearCall(address));
 
-    if (IsDialogAvailable_Func)
+    // DIALOG STUFF
+    uintptr_t address = GW::Scanner::Find("\x89\x4b\x24\x8b\x4b\x28\x83\xe9\x00", "xxxxxxxxx");
+    if (address)
     {
-        GW::HookBase::CreateHook(IsDialogAvailable_Func,
-                                 OnIsDialogAvailable,
-                                 reinterpret_cast<void **>(&IsDialogAvailable_Ret));
-        GW::HookBase::EnableHooks(IsDialogAvailable_Func);
+        SendDialog_Func = (SendDialog_pt)GW::Scanner::FunctionFromNearCall(address + 0x15);
     }
-    if (!IsDialogAvailable_Func)
+    if (!SendDialog_Func)
+    {
+        MessageBox(nullptr, Name(), "Failed to scan for IsDialogAvailable_Func", 0);
+    }
+    if (SendDialog_Func)
+    {
+        GW::HookBase::CreateHook(SendDialog_Func, OnIsDialogAvailable, (void **)&SendDialog_Ret);
+        GW::HookBase::EnableHooks(SendDialog_Func);
+    }
+    if (!SendDialog_Func)
     {
         MessageBox(nullptr, Name(), "Failed to get signature for IsDialogAvailable_Func", 0);
+    }
+
+    // SIGNPOST STUFF
+    address = GW::Scanner::Find("\x89\x4b\x24\x8b\x4b\x28\x83\xe9\x00", "xxxxxxxxx");
+    if (address)
+    {
+        SendSignpostDialog_Func = (SendDialog_pt)GW::Scanner::FunctionFromNearCall(address + 0x15);
+    }
+    if (!SendSignpostDialog_Func)
+    {
+        MessageBox(nullptr, Name(), "Failed to scan for IsDialogAvailable_Func", 0);
+    }
+    if (SendSignpostDialog_Func)
+    {
+        GW::HookBase::CreateHook(SendSignpostDialog_Func, OnIsDialogAvailable, (void **)&SendSignpostDialog_Ret);
+        GW::HookBase::EnableHooks(SendSignpostDialog_Func);
+    }
+    if (!SendSignpostDialog_Func)
+    {
+        MessageBox(nullptr, Name(), "Failed to get signature for IsDialogAvailable_Func", 0);
+    }
+
+    // CTOS STUFF
+    SendPacket_Func =
+        (SendPacket_pt)GW::Scanner::FindAssertion("p:\\code\\net\\msg\\msgconn.cpp", "bytes >= sizeof(dword)", -0x67);
+    if (!SendPacket_Func)
+    {
+        MessageBox(nullptr, Name(), "Failed to scan for SendPacket_Func", 0);
+    }
+    if (SendPacket_Func)
+    {
+        GW::HookBase::CreateHook(SendPacket_Func, CtoSHandler_Func, reinterpret_cast<void **>(&SendPacket_Ret));
+        GW::HookBase::EnableHooks(SendPacket_Func);
+    }
+    if (!SendPacket_Func)
+    {
+        MessageBox(nullptr, Name(), "Failed to get signature for SendPacket_Func", 0);
     }
 }
--- src/LongFinger/LongFinger.cpp
+++ src/LongFinger/LongFinger.cpp
@@ -2,14 +2,19 @@
 
 #include <GWCA/GWCA.h>
 
-#include <GWCA/Packets/Opcodes.h>
-#include <GWCA/Utilities/Hooker.h>
-
+#include <GWCA/Constants/Constants.h>
+#include <GWCA/Constants/Maps.h>
 #include <GWCA/GameEntities/Agent.h>
 #include <GWCA/Managers/AgentMgr.h>
 #include <GWCA/Managers/ChatMgr.h>
 #include <GWCA/Managers/CtoSMgr.h>
 #include <GWCA/Managers/GameThreadMgr.h>
+#include <GWCA/Managers/MapMgr.h>
+#include <GWCA/Packets/Opcodes.h>
+#include <GWCA/Utilities/Hooker.h>
+
+#define GAME_CMSG_INTERACT_GADGET (0x004F)
+#define GAME_CMSG_OPEN_CHEST (0x0051)
 
 
 DLLAPI ToolboxPlugin *ToolboxPluginInstance()
@@ -26,24 +31,25 @@ void LongFinger::Initialize(ImGuiContext *ctx, const ImGuiAllocFns fns, const HM
 
     GW::Chat::CreateCommand(L"longfinger", [this](const wchar_t *, int, LPWSTR *) {
         GW::GameThread::Enqueue([this] {
-            const auto target = GW::Agents::GetTarget();
-            if (target && target->GetIsGadgetType())
+            if (GW::Map::GetInstanceType() == GW::Constants::InstanceType::Explorable)
             {
-                bool res = true;
-
-#define GAME_CMSG_INTERACT_GADGET (0x004F) // 79
-#define GAME_CMSG_OPEN_CHEST (0x0051)      // 81
-
-                res = res && GW::CtoS::SendPacket(0xC, GAME_CMSG_INTERACT_GADGET, target->agent_id, 0);
-                res = res && GW::CtoS::SendPacket(0x8, GAME_CMSG_OPEN_CHEST, 0x2);
-                if (!res)
+                const auto *target = GW::Agents::GetTarget();
+                if (target && target->type == 0x200)
                 {
-                    WriteChat(GW::Chat::CHANNEL_GWCA1, L"Failed to open target locked chest", L"LongFinger");
+                    if (!GW::CtoS::SendPacket(0xC, GAME_CMSG_INTERACT_GADGET, target->agent_id, 0))
+                    {
+                        WriteChat(GW::Chat::CHANNEL_GWCA1, L"Failed to open target locked chest 1", L"LongFinger");
+                    }
+                    if (!GW::CtoS::SendPacket(0x8, GAME_CMSG_OPEN_CHEST, 0x2))
+                    {
+                        WriteChat(GW::Chat::CHANNEL_GWCA1, L"Failed to open target locked chest 2", L"LongFinger");
+                    }
                 }
-            }
-            else
-            {
-                WriteChat(GW::Chat::CHANNEL_GWCA1, L"Target is not a locked chest", L"LongFinger");
+
+                else
+                {
+                    WriteChat(GW::Chat::CHANNEL_GWCA1, L"Target is not a locked chest", L"LongFinger");
+                }
             }
         });
     });
