98146cc7573d70fe20086007b46c6439a5c6a0de
franneck94 <schaffranek.jan@web.de>
2024-06-30 14:29:21 +0200

    updated plugin names

--- cmake/gwtoolboxdll_plugins.cmake
+++ cmake/gwtoolboxdll_plugins.cmake
@@ -1,5 +1,11 @@
 macro(add_tb_plugin PLUGIN)
-  add_library(${PLUGIN} SHARED)
+  if(CMAKE_BUILD_TYPE MATCHES Debug)
+    set(PLUGIN_NAME ${PLUGIN}_debug)
+  else()
+    set(PLUGIN_NAME ${PLUGIN})
+  endif()
+
+  add_library(${PLUGIN_NAME} SHARED)
 
   file(
     GLOB
@@ -12,10 +18,10 @@ macro(add_tb_plugin PLUGIN)
     ${gwtoolboxpp_SOURCE_DIR}/plugins/Base/*.cpp)
 
   target_include_directories(
-    ${PLUGIN} PRIVATE ${gwtoolboxpp_SOURCE_DIR}/plugins/Base
-                      ${gwtoolboxpp_SOURCE_DIR}/GWToolboxdll)
+    ${PLUGIN_NAME} PRIVATE ${gwtoolboxpp_SOURCE_DIR}/plugins/Base
+                           ${gwtoolboxpp_SOURCE_DIR}/GWToolboxdll)
   target_link_libraries(
-    ${PLUGIN}
+    ${PLUGIN_NAME}
     PUBLIC RestClient
            directxtex
            easywsclient
@@ -24,19 +30,20 @@ macro(add_tb_plugin PLUGIN)
            imgui::fonts
            helper_utils)
 
-  target_compile_options(${PLUGIN} PRIVATE /W4 /Gy)
-  target_compile_options(${PLUGIN} PRIVATE $<$<NOT:$<CONFIG:Debug>>:/GL>)
-  target_compile_options(${PLUGIN} PRIVATE $<$<CONFIG:Debug>:/ZI /Od /RTCs>)
-  target_compile_options(${PLUGIN} PRIVATE $<$<CONFIG:RelWithDebInfo>:/Zi>)
+  target_compile_options(${PLUGIN_NAME} PRIVATE /W4 /Gy)
+  target_compile_options(${PLUGIN_NAME} PRIVATE $<$<NOT:$<CONFIG:Debug>>:/GL>)
+  target_compile_options(${PLUGIN_NAME} PRIVATE $<$<CONFIG:Debug>:/ZI /Od
+                                                /RTCs>)
+  target_compile_options(${PLUGIN_NAME} PRIVATE $<$<CONFIG:RelWithDebInfo>:/Zi>)
 
   set_target_properties(
-    ${PLUGIN}
+    ${PLUGIN_NAME}
     PROPERTIES VS_GLOBAL_RunCodeAnalysis false
                VS_GLOBAL_EnableMicrosoftCodeAnalysis true
                VS_GLOBAL_EnableClangTidyCodeAnalysis false)
 
   target_link_options(
-    ${PLUGIN}
+    ${PLUGIN_NAME}
     PRIVATE
     /WX
     /OPT:REF
@@ -52,15 +59,15 @@ macro(add_tb_plugin PLUGIN)
   )
 
   source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}" FILES ${SOURCES})
-  target_sources(${PLUGIN} PRIVATE ${SOURCES})
+  target_sources(${PLUGIN_NAME} PRIVATE ${SOURCES})
   target_compile_definitions(
-    ${PLUGIN} PRIVATE "NOMINMAX" "_WIN32_WINNT=_WIN32_WINNT_WIN7"
-                      "WIN32_LEAN_AND_MEAN" "VC_EXTRALEAN" "BUILD_DLL")
+    ${PLUGIN_NAME} PRIVATE "NOMINMAX" "_WIN32_WINNT=_WIN32_WINNT_WIN7"
+                           "WIN32_LEAN_AND_MEAN" "VC_EXTRALEAN" "BUILD_DLL")
 
-  set_target_properties(${PLUGIN} PROPERTIES FOLDER "src")
+  set_target_properties(${PLUGIN_NAME} PROPERTIES FOLDER "src")
 
   if(${ENABLE_WARNINGS})
-    target_set_warnings(TARGET ${PLUGIN} ENABLE ${ENABLE_WARNINGS} AS_ERRORS
-                        ${ENABLE_WARNINGS_AS_ERRORS})
+    target_set_warnings(TARGET ${PLUGIN_NAME} ENABLE ${ENABLE_WARNINGS}
+                        AS_ERRORS ${ENABLE_WARNINGS_AS_ERRORS})
   endif()
 endmacro()
